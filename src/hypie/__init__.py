1d2f8b8ba1c9f555db28697c76f398a8cd9b5fa3
Yasser <yasserfawzi1029@outlook.com>
2026-07-15 03:01:33 +0300

    build(python version): downgrade to 3.12+

--- src/hypie/__init__.py
+++ src/hypie/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass, field
 
 from hypie.hs_ast.features import Feature
--- src/hypie/hs_ast/expressions.py
+++ src/hypie/hs_ast/expressions.py
@@ -1,7 +1,8 @@
+from __future__ import annotations
+
 from dataclasses import dataclass, field
 from typing import Literal, get_args
 import json
-
 from functools import wraps
 
 from textwrap import indent
